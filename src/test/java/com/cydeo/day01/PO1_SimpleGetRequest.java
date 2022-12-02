985868d854db39c8b04f087445b0e8c0483444c2
Umid <111351596+vSunShineJk@users.noreply.github.com>
2022-12-03 00:47:37 +0500

    ay01&day02

--- src/test/java/com/cydeo/day01/PO1_SimpleGetRequest.java
+++ src/test/java/com/cydeo/day01/PO1_SimpleGetRequest.java
@@ -1,2 +1,35 @@
-package com.cydeo.day01;public class PO1_SimpleGetRequest {
+package com.cydeo.day01;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PO1_SimpleGetRequest {
+
+    String url = "http://34.203.212.11:8000/api/spartans";
+
+    /**
+     * When user request to /opi.spartans endpoint
+     * then user should be able to see status code is 200
+     * And print out responses body into screen
+     */
+
+    @Test
+    public void simpleGetRequest(){
+        // send request to the url and get response as Response interface
+        Response response = RestAssured.get(url);
+
+        // both same, no difference
+        System.out.println("response.getStatusCode() = " + response.getStatusCode());
+        System.out.println("response.statusCode() = " + response.statusCode());
+        System.out.println("response.statusLine() = " + response.statusLine());
+
+        int actualStatusCode = response.getStatusCode();
+        Assertions.assertEquals(200,actualStatusCode);
+
+        // how to print into screen?
+        response.prettyPrint();
+    }
+
 }
--- src/test/java/com/cydeo/day02/PO1_SpartanGetRequests.java
+++ src/test/java/com/cydeo/day02/PO1_SpartanGetRequests.java
@@ -1,2 +1,39 @@
-package com.cydeo.day02;public class PO1_SpartanGetRequests {
+package com.cydeo.day02;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PO1_SpartanGetRequests {
+
+    String url = "http://34.203.212.11:8000";
+
+    /*
+     * Given accept content type is application/json
+     * When user sends GET request /api/spartans endpoint
+     * Then status code should be 200
+     * And content type should be application json
+     */
+
+    @Test
+    public void getAllSpartans(){
+        Response response = RestAssured
+                                    .given()
+                                    .accept(ContentType.JSON)
+                                    .when()
+                                    .get(url + "/api/spartans");
+        // print response
+        response.prettyPrint();
+
+        // how to get status code
+        int actualStatusCode = response.statusCode();
+        Assertions.assertEquals(200,actualStatusCode);
+
+        // how to get contentType
+        String actualContentType = response.contentType();
+        Assertions.assertEquals("application/json",actualContentType);
+    }
+
 }
