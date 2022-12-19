c68960b4ff119f05d4d9e99614916a0b872b7b00
Umid <111351596+vSunShineJk@users.noreply.github.com>
2022-12-19 22:58:21 +0500

    day09

---
+++ src/test/java/com/cydeo/day09/P01_SpartanXMLTest.java
@@ -0,0 +1,49 @@
+package com.cydeo.day09;
+
+import com.cydeo.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class P01_SpartanXMLTest extends SpartanTestBase {
+
+    /*
+    Given accept type application/xml
+    When send request to /api/spartans
+    Then status code 200
+     */
+    @Test
+    public void test1(){
+        given().accept(ContentType.XML)
+                .auth().basic("admin","admin")
+                .get("/api/spartans")
+                .prettyPeek()
+                .then()
+                .statusCode(200)
+                .contentType("application/xml")
+                .body("List.item[0].name",is("Paige"))
+                .body("List.item[0].gender",is("Female"));
+    }
+
+    @DisplayName("GET /api/spartans with using XMLPath")
+    @Test
+    public void test2(){
+        Response response = given().accept(ContentType.XML)
+                .auth().basic("admin", "admin")
+                .get("/api/spartans");
+
+        XmlPath xmlPath = response.xmlPath();
+
+        System.out.println("xmlPath.getString(\"List.item[0].name\") = " + xmlPath.getString("List.item[0].name"));
+        System.out.println("xmlPath.getString(\"List.item[0].gender\") = " + xmlPath.getString("List.item[0].gender"));
+        System.out.println("xmlPath.getString(\"List.item[-1].name\") = " + xmlPath.getString("List.item[-1].name"));
+        System.out.println("xmlPath.getString(\"List.item.name\") = " + xmlPath.get("List.item.name"));
+        System.out.println("xmlPath.getList(\"List.item\").size() = " + xmlPath.getList("List.item").size());
+
+    }
+}
